457158111d77f45e8b963e25637d908760d49631
pranjal <pranjalsingh2022sep@cedcoss.com>
2022-09-26 11:52:09 +0530

    js variable exercise 4

--- SRC/script.js
+++ SRC/script.js
@@ -2,7 +2,12 @@
 var carName = "Volvo";
 // js variable exercise 2
 var x = 50;
-//js variable exercise 3 
+//js variable exercise 3
 var x = 5;
 var y = 10;
 document.getElementById("demo").innerHTML = x + y;
+// js variable exercise 4
+var x = 5;
+var y = 10;
+var z = x + y;
+alert(z);
